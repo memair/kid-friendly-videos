03ed2c7ac625977764faab85649276f4a5727284
gregology <greg@gho.st>
2019-02-21 19:28:28 -0500

    trying recommendaiton bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -84,7 +84,7 @@ class User < ApplicationRecord
     """
 
     results = ActiveRecord::Base.connection.execute(sql).to_a
-    results.each_with_index.map { |r, idx| Recommendation.new(video: Video.find(r['id']), priority: priority, expires_at: r['expires_at'], thumbnail_url: r['thumbnail_url'], published_at: r['published_at'], duration: r['duration']) }
+    results.map { |r| Recommendation.new(video: Video.find(r['id']), priority: priority, expires_at: r['expires_at'], thumbnail_url: r['thumbnail_url'], published_at: r['published_at'], duration: r['duration']) }
   end
 
   def setup?
