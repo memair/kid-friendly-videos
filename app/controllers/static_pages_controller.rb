9f9a6d40138d4dee3179209899b1bad2151ce952
gregology <greg@gho.st>
2019-02-26 16:29:54 -0500

    fixing if logic

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
   before_action :flash_notice
 
   def home
-    flash[:notice] = "Update your child's deatils below to start recommendations" unless !!current_user && current_user.setup?
+    flash[:notice] = "Update your child's deatils below to start recommendations" if !!current_user && !current_user.setup?
   end
 
   def flash_notice
