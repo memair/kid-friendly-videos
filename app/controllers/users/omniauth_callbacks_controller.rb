0294cc72753f81bfb02364a8683b208e99b1c7e0
gregology <greg@gho.st>
2019-02-28 11:04:31 -0500

    redirecting to root with flash message when not authorized

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -7,7 +7,12 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       redirect_to root_path
     else
       session['devise.memair_data'] = request.env['omniauth.auth'].except(:extra) # Removing extra as it can overflow some session stores
-      redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
+      redirect_to root_path, alert: @user.errors.full_messages.join("\n")
     end
   end
+
+  def failure
+    flash[:error] = 'You must authorize Kid Friendly Videos so that it can make recommendations in the Memair Player'
+    redirect_to root_path
+ end
 end
